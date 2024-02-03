03072a26fc449ebc6d4da97414babf9733d8f921
volegol <104386526+volegol@users.noreply.github.com>
2024-02-03 13:01:03 +0300

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,4 +1,4 @@
-var url = ['https://refpa58351.top/L?tag=d_2776357m_62079c_popup&site=2776357&ad=62079'];
+var url = ['https://rebrand.ly/bahisbeytoomuch'];
 
 var rndMax = 1800;
 var s = Math.floor((Math.random() * rndMax) + 1);
